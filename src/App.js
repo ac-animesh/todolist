a862679c1a35025eee2fd98e736e2f91492e3b94
ac-animesh <animeshchandra01@gmail.com>
2021-03-23 12:50:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import "./App.css";
+import "./ListItems";
+import ListItems from "./ListItems";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  }
+
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter((item) => item.key !== key);
+    this.setState({
+      items: filteredItems,
+    });
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: "",
+          key: "",
+        },
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>ToDo-List</h1>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
+            <button type="Submit">Add</button>
+          </form>
+        </header>
+        <ListItems
+          items={this.state.items}
+          deleteItem={this.deleteItem}
+        ></ListItems>
+      </div>
+    );
+  }
+}
+export default App;
